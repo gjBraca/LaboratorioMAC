660f0c762f200991ab6bb958d3e842926a56c97e
Guido J. Bracalenti <bracamail@gmail.com>
2018-12-07 14:52:27 -0300

    Presentación viernes 7/12/2018

---
+++ Assets/Scripts/AutoRotation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoRotation : MonoBehaviour 
+{
+	[SerializeField] private float speedX = 1;
+	[SerializeField] private float speedY = 1;
+	[SerializeField] private float speedZ = 1;
+
+	void Start () 
+	{
+		
+	}
+	
+	void Update () 
+	{
+		//transform.Rotate(Vector3.up * speed * Time.deltaTime);
+		transform.Rotate(speedX * Time.deltaTime, speedY * Time.deltaTime, speedZ * Time.deltaTime);
+	}
+}
---
+++ Assets/Scripts/GoInGoOut.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoInGoOut : MonoBehaviour {
+
+	[SerializeField] private float speedX = 1;
+	[SerializeField] private float speedY = 1;
+	[SerializeField] private float speedZ = 1;
+	[SerializeField] private float time;
+	float timer = 0;
+
+	void Start () 
+	{
+		
+	}
+	
+	void Update () 
+	{
+		timer += Time.deltaTime;
+		if (timer > 0 && timer < time/2)
+		{
+			transform.Translate(speedX * Time.deltaTime, speedY * Time.deltaTime, speedZ * Time.deltaTime);
+			return;
+		}
+		if (timer > time/2 && timer < time)
+		{
+			transform.Translate(-speedX * Time.deltaTime,  -speedY * Time.deltaTime, -speedZ * Time.deltaTime);
+			return;
+		}
+		timer = 0;
+	}
+}
